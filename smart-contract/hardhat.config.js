27939da78b89f1b74f1c106771e7ef3f84ca94dc
Hung Pham <phamhungbkhn@gmail.com>
2021-12-09 17:26:58 +0700

    misc: add hardhat checkCurrentBlock task

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -11,6 +11,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+// task action function receives the Hardhat Runtime Environment as second argument
+task(
+    "blockNumber",
+    "Prints the current block number",
+    async (_, { ethers }) => {
+      await hre.ethers.provider.getBlockNumber().then((blockNumber) => {
+        console.log("Current block number: " + blockNumber);
+      });
+    }
+);
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
