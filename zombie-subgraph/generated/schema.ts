39df52e1b6e93c9b71685925db08b7f1748fa311
Hung Pham <phamhungbkhn@gmail.com>
2021-12-10 14:57:07 +0700

    update: first version subgraph

--- zombie-subgraph/generated/schema.ts
+++ zombie-subgraph/generated/schema.ts
@@ -11,31 +11,31 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class Gravatar extends Entity {
+export class ExampleEntity extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
 
+    this.set("count", Value.fromBigInt(BigInt.zero()));
     this.set("owner", Value.fromBytes(Bytes.empty()));
-    this.set("displayName", Value.fromString(""));
-    this.set("imageUrl", Value.fromString(""));
+    this.set("approved", Value.fromBytes(Bytes.empty()));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save Gravatar entity without an ID");
+    assert(id != null, "Cannot save ExampleEntity entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save Gravatar entity with non-string ID. " +
+        "Cannot save ExampleEntity entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("Gravatar", id.toString(), this);
+      store.set("ExampleEntity", id.toString(), this);
     }
   }
 
-  static load(id: string): Gravatar | null {
-    return changetype<Gravatar | null>(store.get("Gravatar", id));
+  static load(id: string): ExampleEntity | null {
+    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
   }
 
   get id(): string {
@@ -47,6 +47,15 @@ export class Gravatar extends Entity {
     this.set("id", Value.fromString(value));
   }
 
+  get count(): BigInt {
+    let value = this.get("count");
+    return value!.toBigInt();
+  }
+
+  set count(value: BigInt) {
+    this.set("count", Value.fromBigInt(value));
+  }
+
   get owner(): Bytes {
     let value = this.get("owner");
     return value!.toBytes();
@@ -56,21 +65,12 @@ export class Gravatar extends Entity {
     this.set("owner", Value.fromBytes(value));
   }
 
-  get displayName(): string {
-    let value = this.get("displayName");
-    return value!.toString();
+  get approved(): Bytes {
+    let value = this.get("approved");
+    return value!.toBytes();
   }
 
-  set displayName(value: string) {
-    this.set("displayName", Value.fromString(value));
-  }
-
-  get imageUrl(): string {
-    let value = this.get("imageUrl");
-    return value!.toString();
-  }
-
-  set imageUrl(value: string) {
-    this.set("imageUrl", Value.fromString(value));
+  set approved(value: Bytes) {
+    this.set("approved", Value.fromBytes(value));
   }
 }
--- zombie-subgraph/src/mapping.ts
+++ zombie-subgraph/src/mapping.ts
@@ -1,22 +1,73 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  ZombieOwnership,
+  Approval,
+  ApprovalForAll,
+  NewZombie,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/ZombieOwnership/ZombieOwnership"
+import { ExampleEntity } from "../generated/schema"
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.balanceOf(...)
+  // - contract.countZombie(...)
+  // - contract.getApproved(...)
+  // - contract.getZombiesByOwner(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.name(...)
+  // - contract.owner(...)
+  // - contract.ownerOf(...)
+  // - contract.supportsInterface(...)
+  // - contract.symbol(...)
+  // - contract.tokenURI(...)
+  // - contract.uri(...)
+  // - contract.zombieToOwner(...)
+  // - contract.zombies(...)
 }
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleNewZombie(event: NewZombie): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handleTransfer(event: Transfer): void {}
